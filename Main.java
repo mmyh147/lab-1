103e0b7446180d094bbf74b67e22fdc421b7f2b3
mmyh147 <61750916+mmyh147@users.noreply.github.com>
2024-02-14 16:11:29 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,309 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+
+
+
+        //================ Lab1 Task 1 =================
+
+//        int num1, num2;
+//
+//        System.out.println("Enter first number");
+//        num1 = input.nextInt();
+//        System.out.println("Enter second number");
+//        num2 = input.nextInt();
+//
+//        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+//        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+//        System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
+//        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+//        System.out.println(num1 + " mod " + num2 + " = " + (num1 % num2));
+
+
+
+        //================ Lab1 Task 2 =================
+
+//        System.out.println("Enter number: ");
+//        int num = input.nextInt();
+//
+//        for (int i = 1; i <= 10; i++){
+//            System.out.println(num + " x " + i + " = " + (num * i));
+//        }
+
+
+
+        //================ Lab1 Task 3 =================
+
+//        System.out.print("Enter the radius of the circle: ");
+//        double radius = input.nextDouble();
+//
+//        System.out.println("perimeter is = " + (2 * Math.PI * radius));
+//        System.out.println("Area is = " + (Math.PI * (radius * radius)));
+
+
+
+        //================ Lab1 Task 4 =================
+
+//        int num = 0, sum = 0, count;
+//
+//        System.out.println("Enter the counts of numbers");
+//        count = input.nextInt();
+//        for(int i = 1 ; count >= i ; i++ ){
+//
+//            System.out.println("Enter an integer: ");
+//            num = input.nextInt();
+//            sum += num;
+//
+//        }
+//        if(count > 0) {
+//            System.out.println((float) sum / count);
+//        }
+
+
+        //================ Lab1 Task 5 =================
+
+//        int num1, num2, num3;
+//
+//        System.out.println("Enter the first number: ");
+//        num1= input.nextInt();
+//        System.out.println("Enter the second number: ");
+//        num2 = input.nextInt();
+//        System.out.println("Enter the third number: ");
+//        num3 = input.nextInt();
+//
+//        if((num1 + num2) == num3){
+//            System.out.println("the result is: true ");
+//        }else {
+//            System.out.println("the result is: false ");
+//
+//        }
+
+        //================ Lab1 Task 6 =================
+
+//        System.out.println("enter the sentence to reverse: ");
+//        String sentence = input.nextLine();
+//        String reversed = "";
+//        for (int i = sentence.length() - 1; i >= 0; i--) {
+//            reversed += sentence.charAt(i);
+//        }
+//        System.out.println("the sentence reversed: " + reversed);
+
+
+        //================ Lab1 Task 7 =================
+
+//        System.out.println("Enter the number: ");
+//        int num = input.nextInt();
+//
+//        if(num % 2 ==0){
+//            System.out.println("the number " + num + " is Even");
+//        }else{
+//            System.out.println("the number " + num + " is Odd");
+//
+//        }
+
+
+        //================ Lab1 Task 8 =================
+
+//        System.out.println("enter the temperature in Centigrade: ");
+//        int c = input.nextInt();
+//        System.out.println(c * 1.8 + 32);
+
+
+        //================ Lab1 Task 9 =================
+
+//        System.out.println("Enter String: ");
+//        String s = input.nextLine();
+//        System.out.println("Enter number: ");
+//        int i = input.nextInt();
+//
+//        System.out.println(s.charAt(1));
+
+
+        //================ Lab1 Task 10 =================
+//        double width, height;
+//
+//        System.out.println("Enter the width of rectangle: ");
+//        width = input.nextDouble();
+//        System.out.println("Enter the height of rectangle: ");
+//        height = input.nextDouble();
+//        System.out.println("Area is " + width + " * " + height + " = " + (width * height));
+//        System.out.println("perimeter is " + " 2 (" + width + " + " + height + ") = " + 2 * (width + height));
+
+
+
+
+
+
+        //================ Lab1 Task 11 =================
+
+//        int num1, num2;
+//
+//        System.out.println("Enter the firs number: ");
+//        num1 = input.nextInt();
+//        System.out.println("Enter the second number: ");
+//        num2 = input.nextInt();
+//
+//        System.out.println("===== output =====");
+//        if(num1 == num2){
+//            System.out.println(num1 + " == " + num2);
+//        }
+//        if(num1 != num2){
+//            System.out.println(num1 + " != " + num2);
+//        }
+//        if(num1 < num2){
+//            System.out.println(num1 + " < " + num2);
+//        }
+//        if(num1 <= num2){
+//            System.out.println(num1 + " <= " + num2);
+//        }
+//        if(num1 > num2){
+//            System.out.println(num1 + " > " + num2);
+//        }
+//        if(num1 >= num2){
+//            System.out.println(num1 + " >= " + num2);
+//        }
+
+        //================ Lab1 Task 12 =================
+
+//        System.out.println("Enter the seconds: ");
+//        int seconds = input.nextInt();
+//        System.out.println((seconds / 3600) + ":" + ((seconds % 3600) / 60) + ":" + (seconds % 60));
+
+
+        //================ Lab1 Task 13 =================
+
+
+//        int num1, num2, num3, num4;
+//        System.out.println("Enter the first number: ");
+//        num1 = input.nextInt();
+//        System.out.println("Enter the second number: ");
+//        num2 = input.nextInt();
+//        System.out.println("Enter the third number: ");
+//        num3 = input.nextInt();
+//        System.out.println("Enter the fourth number: ");
+//        num4 = input.nextInt();
+//
+//        if(num1 == num2){
+//            if(num1 == num3){
+//                if(num1 == num4){
+//                    System.out.println("Numbers are equal! ");
+//
+//                }else{
+//                    System.out.println("Numbers are not equal! ");
+//                }
+//            }
+//            else{
+//                System.out.println("Numbers are not equal! ");
+//            }
+//        }
+//        else {
+//            System.out.println("Numbers are not equal! ");
+//        }
+
+
+        //================ Lab1 Task 14 =================
+
+//        System.out.println("Enter the number: ");
+//        int num = input.nextInt();
+//        if(num > 0){
+//            System.out.println("the number is positive");
+//        }else if(num < 0){
+//            System.out.println("the number is negative");
+//        }else{
+//            System.out.println("the number is Zero ");
+//        }
+
+        //================ Lab1 Task 15 =================
+
+
+//        int num = 0, pos = 0, neg = 0, zero = 0;
+//        do {
+//            System.out.println("Enter the number: ");
+//            num = input.nextInt();
+//            if(num == -1){
+//                break;
+//            }
+//            else if(num > 0){
+//                pos++;
+//            } else if (num < 0) {
+//                neg++;
+//            }else{
+//                zero++;
+//            }
+//
+//        }while (true);
+//
+//        System.out.println("Positives: " + pos);
+//        System.out.println("Zero: " + zero);
+//        System.out.println("Negatives: " + neg);
+
+
+        //================ Lab1 Task 16 =================
+
+
+//                System.out.print("Enter an integer: ");
+//                int num = input.nextInt();
+//                int reversedNumber = 0;
+//
+//                while (num != 0) {
+//                    int digit = num % 10;
+//                    reversedNumber = reversedNumber * 10 + digit;
+//                    num /= 10;
+//                }
+//                System.out.println("Reversed number: " + reversedNumber);
+
+
+
+        //================ Lab1 Task 17 =================
+
+//
+//        int max = 0, min = 0;
+//        String num;
+//
+//        System.out.println("Enter the number: ");
+//        num = input.nextLine();
+//        max = Integer.parseInt(num);
+//        min = Integer.parseInt(num);
+//
+//        do {
+//            System.out.println("Enter the number or enter exit to finish: ");
+//            num = input.nextLine();
+//
+//            if((num.equalsIgnoreCase("exit"))){
+//                break;
+//            }
+//            else if (Integer.parseInt(num) > max){
+//                max = Integer.parseInt(num);
+//            }else if(Integer.parseInt(num) < min){
+//                min = Integer.parseInt(num);
+//            }
+//        }while (true);
+//
+//        System.out.println("The largest number: " + max);
+//        System.out.println("The smallest number: " + min);
+
+
+
+        //================ Lab1 Task 18 =================
+
+
+//                System.out.print("Enter a string: ");
+//                String inputString = input.nextLine();
+//                int count = 0;
+//                for (int i = 0; i < inputString.length(); i++) {
+//                    if (inputString.charAt(i) == 'a' || inputString.charAt(i) == 'A') {
+//                        count++;
+//                    }
+//                }
+//                System.out.println("Number of a's: " + count);
+
+
+
+
+
+    }
+}
